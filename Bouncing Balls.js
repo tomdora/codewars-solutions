25e50d0f2c0586d34a9dae4d15911124eca5228b
Tom Windows <engineertf@gmail.com>
2021-06-28 14:50:11 -0400

    Update Bouncing Balls.js
    Misplaced a single mathematical sign, solution works properly now.

--- Bouncing Balls.js
+++ Bouncing Balls.js
@@ -1,5 +1,5 @@
 const bouncingBall = (h, bounce, window) => {
-    if (h <= 0 || bounce <= 0 || bounce > 1 || window >= h) return -1;
+    if (h <= 0 || bounce <= 0 || bounce >= 1 || window >= h) return -1;
     let sightings = 0;
     while (true) {
         sightings += 1;
